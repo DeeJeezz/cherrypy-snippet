b38bcb989a55f248816f5982ee851f203558da11
GrobIvanovich <killmaster00431@gmail.com>
2020-06-24 15:04:13 +0500

    Added index api endpoint

--- api/root.py
+++ api/root.py
@@ -4,6 +4,7 @@ import cherrypy
 class ApiRoot(object):
 
     @cherrypy.expose()
+    @cherrypy.tools.json_out()
     def index(self):
 
-        return {}
+        return {'error': None, 'result': 'This is index endpoint!'}
--- webservice/root.py
+++ webservice/root.py
@@ -4,7 +4,6 @@ import cherrypy
 class WebServiceRoot(object):
 
     @cherrypy.expose()
-    # @cherrypy.tools.json_out()
     @cherrypy.tools.render(template_name='index.html')
     def index(self):
         return {
